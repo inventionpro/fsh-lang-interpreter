372e09e71ea928042164e834c280cbc996900598
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-09-05 22:36:19 +0200

    Update interpreter.js

--- interpreter.js
+++ interpreter.js
@@ -1,35 +1,49 @@
 let logs = document.getElementById('logs');
 function log(text) {
-  logs.value += text+'\n'
+  logs.value += text+'\n';
 }
 
-function readType(val) {
-  if (!isNaN(val)) {
-    return {
-      value: Number(val.split(' ')[0]),
-      type: 'number'
-    };
-  }
-  if (!val) {
-    return {
-      value: null,
-      type: 'null'
-    };
-  }
-  if ((val.match(/^'[^']*'$/m)||[false])[0] || (val.match(/^"[^"]*"$/m)||[false])[0] || (val.match(/^`[^`]*`$/m)||[false])[0]) {
-    return {
-      value: val.slice(1,-1),
-      type: 'string'
-    };
-  }
-  return {
-    value: val,
-    type: 'UNKNOWN'
-  }
+let out = document.getElementById('output');
+function output(text) {
+  out.value += text+'\n';
 }
 
 function interpret(code) {
   logs.value = '';
+  out.value = '';
+  let vars = {};
+  function readType(val) {
+    if (!isNaN(val)) {
+      return {
+        value: Number(val),
+        type: 'number'
+      };
+    }
+    if (!val) {
+      return {
+        value: null,
+        type: 'null'
+      };
+    }
+    if ((val.match(/^'[^']*'$/m)||[false])[0] || (val.match(/^"[^"]*"$/m)||[false])[0] || (val.match(/^`[^`]*`$/m)||[false])[0]) {
+      return {
+        value: val.slice(1,-1),
+        type: 'string'
+      };
+    }
+    if (!val.includes(' ')) {
+      if (vars[val].type) {
+        return {
+          value: vars[val].value,
+          type: vars[val].type
+        }
+      }
+    }
+    return {
+      value: val,
+      type: 'UNKNOWN'
+    }
+  }
   log('start> preprocess');
   let preprocess = code
     .split('\n')
@@ -37,7 +51,6 @@ function interpret(code) {
     .filter(l => l.length>0);
   log('end> preprocess');
   log('start> run');
-  let vars = {};
   for (let i = 0; i < preprocess.length; i++) {
     log('start> line '+i);
     let args = preprocess[i].split(' ').map(t=>t.trim()).filter(t=>t.length>0);
@@ -45,17 +58,49 @@ function interpret(code) {
       case 'let':
       case 'const':
         if(args[2] !== '=') {
+          if (!args[3]) {
+            if (args[0] === 'const') {
+              log('error> tried initializing a constant with no value');
+              output('Error: Tried initializing a constant with no value');
+              continue;
+            }
+            vars[args[1]] = { value: null, type: 'null', mut: 'let' };
+            continue;
+          }
           log('error> expected = recived '+args[2]);
+          output('Error: Expected = recived '+args[2]);
           continue;
         }
         let con = readType(args.slice(3, args.length).join(' '));
         if (con.type === 'UNKNOWN') {
           log('error> unknown type suplied, recived '+con.value);
+          output('Error: Unknown type suplied, recived '+con.value);
           continue;
         }
         vars[args[1]] = { value: con.value, type: con.type, mut: args[0] };
+        break;
+      case 'print':
+        if(args[1]) {
+          log('error> unknown exta data passed after print');
+          output('Error: Unknown exta data passed after print');
+          continue;
+        }
+        if ((args[0].match(/^print\([^¬]*\)$/m)||[false])[0]) {
+          let con = readType(args[0].slice(6,-1));
+          if (con.type === 'UNKNOWN') {
+            log('error> unknown type suplied, recived '+con.value);
+            output('Error: Unknown type suplied, recived '+con.value);
+            continue;
+          }
+          output(con.value);
+        } else {
+          log('error> unknown data suplied, recived '+args[0]);
+          output('Error: Unknown data suplied, recived '+args[0]);
+          continue;
+        }
       default:
         log('error> unknown keyword')
+        break;
     }
     log('end> line '+i);
   }
