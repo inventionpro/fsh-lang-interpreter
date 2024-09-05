d94b23b08ef8149eeecd239c630e5111c2ef03f7
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-09-05 21:49:45 +0200

    Update interpreter.js

--- interpreter.js
+++ interpreter.js
@@ -4,13 +4,28 @@ function log(text) {
 }
 
 function readType(val) {
+  if (!isNaN(val)) {
+    return {
+      value: Number(val.split(' ')[0]),
+      type: 'number'
+    };
+  }
   if (!val) {
     return {
       value: null,
       type: 'null'
-    }
+    };
   }
-  return {}
+  if (val.match(/^'[^']*'$/m)[0] || val.match(/^"[^"]*"$/m)[0] || val.match(/^`[^`]*`$/m)[0]) {
+    return {
+      value: val.slice(1,-1),
+      type: 'string'
+    };
+  }
+  return {
+    value: val,
+    type: 'UNKNOWN'
+  }
 }
 
 function interpret(code) {
@@ -28,11 +43,19 @@ function interpret(code) {
     let args = preprocess[i].split(' ').map(t=>t.trim()).filter(t=>t.length>0);
     switch (preprocess[i].match(/^[a-zA-Z0-9]+/m)[0]) {
       case 'let':
+      case 'const':
         if(args[2] !== '=') {
-          log('error> expected = revived '+args[2])
+          log('error> expected = recived '+args[2]);
+          continue;
         }
-        let con = readType(args.slice(3, args.length).join(' '))
-        vars[args[1]] = { value: con.value, type: con.type, mut: 'let' };
+        let con = readType(args.slice(3, args.length).join(' '));
+        if (con.type === 'UNKNOWN') {
+          log('error> unknown type suplied, recived '+con.value);
+          continue;
+        }
+        vars[args[1]] = { value: con.value, type: con.type, mut: args[0] };
+      default:
+        log('error> unknown keyword')
     }
     log('end> line '+i);
   }
