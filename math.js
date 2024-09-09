d14df0194fc243efbeb65862cd9bc16196c2e121
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-09-09 22:39:42 +0200

    Create math.js

---
+++ math.js
@@ -0,0 +1,93 @@
+function evaluate(expression) {
+    // Step 1: Tokenize the expression
+    function tokenize(exp) {
+        const tokens = [];
+        let numberBuffer = '';
+        
+        for (let char of exp) {
+            if (/\d/.test(char)) { // If the character is a digit, build a number
+                numberBuffer += char;
+            } else {
+                if (numberBuffer.length > 0) {
+                    tokens.push(Number(numberBuffer)); // Push the built number as token
+                    numberBuffer = '';
+                }
+                if (/[+\-*/()]/.test(char)) {
+                    tokens.push(char); // Push operators and parentheses as tokens
+                }
+            }
+        }
+        if (numberBuffer.length > 0) {
+            tokens.push(Number(numberBuffer));
+        }
+        return tokens;
+    }
+
+    // Step 2: Convert to Reverse Polish Notation (Shunting Yard Algorithm)
+    function toRPN(tokens) {
+        const outputQueue = [];
+        const operatorStack = [];
+        
+        const precedence = { '+': 1, '-': 1, '*': 2, '/': 2 };
+        const associativity = { '+': 'L', '-': 'L', '*': 'L', '/': 'L' };
+
+        for (let token of tokens) {
+            if (typeof token === 'number') {
+                outputQueue.push(token);
+            } else if (/[+\-*/]/.test(token)) {
+                while (
+                    operatorStack.length &&
+                    /[+\-*/]/.test(operatorStack[operatorStack.length - 1]) &&
+                    (
+                        (associativity[token] === 'L' && precedence[token] <= precedence[operatorStack[operatorStack.length - 1]]) ||
+                        (associativity[token] === 'R' && precedence[token] < precedence[operatorStack[operatorStack.length - 1]])
+                    )
+                ) {
+                    outputQueue.push(operatorStack.pop());
+                }
+                operatorStack.push(token);
+            } else if (token === '(') {
+                operatorStack.push(token);
+            } else if (token === ')') {
+                while (operatorStack.length && operatorStack[operatorStack.length - 1] !== '(') {
+                    outputQueue.push(operatorStack.pop());
+                }
+                operatorStack.pop(); // Pop the '('
+            }
+        }
+
+        while (operatorStack.length) {
+            outputQueue.push(operatorStack.pop());
+        }
+
+        return outputQueue;
+    }
+
+    // Step 3: Evaluate the RPN expression
+    function evaluateRPN(rpn) {
+        const stack = [];
+        
+        for (let token of rpn) {
+            if (typeof token === 'number') {
+                stack.push(token);
+            } else if (/[+\-*/]/.test(token)) {
+                const b = stack.pop();
+                const a = stack.pop();
+                switch (token) {
+                    case '+': stack.push(a + b); break;
+                    case '-': stack.push(a - b); break;
+                    case '*': stack.push(a * b); break;
+                    case '/': stack.push(a / b); break;
+                }
+            }
+        }
+        
+        return stack[0];
+    }
+
+    const tokens = tokenize(expression);
+    const rpn = toRPN(tokens);
+    return evaluateRPN(rpn);
+}
+
+default export evaluate
